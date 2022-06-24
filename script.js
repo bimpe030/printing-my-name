fd0ac2b7b6622e3f8e1f91a1c97a0a2f29b89f9d
bimpe030 <adelanwa.zainab05@gmail.com>
2022-06-24 23:30:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function myDetails(){
+    let name = "Adelanwa Zainab Adebimpe"
+    let height = "140cm"
+    let country = "Nigeria"
+
+    const profile = "My name is:" + " " +  name +
+     "\nMy height is:" + " " + height + "\nI am from:" + " " + country;
+
+     alert(profile)
+};
+
+myDetails()
